94094e5cf7db89a3b17b323494262f24f4bcc37c
Yucheol Shin <yshin@amnh.org>
2025-09-15 10:47:56 -0400

    commit BUSCO slurm job script

--- Shell/O_rufo_busco.sh
+++ Shell/O_rufo_busco.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name yshin_oocatochus_busco
-#SBATCH --nodes=10
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=30
 #SBATCH --mem=100gb
 #SBATCH --time=100:00:00
 #SBATCH --mail-type=ALL
@@ -13,5 +14,7 @@
 source ~/.bash_profile
 conda activate mytools
 
+cd /home/yshin/mendel-nas1/snake_genome_ass/Oocatochus/Shell/busco_outfiles/
+
 Oocatochus_assembly="/home/yshin/mendel-nas1/snake_genome_ass/Oocatochus/Shell/hifiasm_outfiles/Oocatochus_rufodorsatus_v1.asm.bp.p_ctg.fa"
-busco -m genome -i $Oocatochus_assembly -o /home/yshin/mendel-nas1/snake_genome_ass/Oocatochus/Shell/busco_outfiles/Oocatochus_BUSCO -l /home/yshin/mendel-nas1/snake_genome_ass/busco/sauropsida_odb10 -f --metaeuk --offline --download_path /home/yshin/mendel-nas1/snake_genome_ass/busco
+busco -m genome -i $Oocatochus_assembly -o Oocatochus_BUSCO -l /home/yshin/mendel-nas1/snake_genome_ass/busco/sauropsida_odb10 -f --metaeuk --offline --download_path /home/yshin/mendel-nas1/snake_genome_ass/busco
